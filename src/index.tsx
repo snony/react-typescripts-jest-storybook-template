0b6900bb07ed6935024b5bf219fdfcc55dfdb238
Moise Lubwimi <moise.lubwimi@weareadaptive.com>
2019-04-26 11:36:02 +0100

    new some test

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,7 @@ import App from './App';
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 
+export const foo = 5
 ReactDOM.render(
   <App />,
   document.getElementById('root') as HTMLElement
