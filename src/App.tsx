6a8cbb177baffc0a381c72d0ff6a2b8c2b2c797d
Moise Lubwimi <moise.lubwimi@weareadaptive.com>
2019-05-17 18:35:33 +0100

    transition playing with css grid

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,42 @@
 import * as React from 'react';
 import './App.css';
+import styled from 'styled-components/macro'
 
-import logo from './logo.svg';
+
+const FooStyle = styled.div`
+  min-height: 100%;
+  display: grid;
+  grid-auto-flow: column;
+  grid-gap: 5px 5px;
+  // grid-template-columns: 2px 3fr 2px 1fr 2px;
+  // grid-template-rows: 2px auto 2px;
+`
+
+const SomeStyle = styled.div`
+// grid-column-start: 2;
+// grid-column-end: 3;
+background-color: blue;
+// grid-row-start: 2;
+// grid-row-end: 3;
+`
+
+export const OtherContent = styled.div`
+  // grid-area: otherContent;
+  background-color: red;
+  // grid-column-start: 4;
+  // grid-column-end: 5;
+  // grid-row-start: 2;
+  // grid-row-end: 3;
+`
+
 
 class App extends React.Component {
   public render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
-      </div>
+      <FooStyle>
+        <SomeStyle>Click On me</SomeStyle>
+        <OtherContent>This is Two</OtherContent>
+      </FooStyle>
     );
   }
 }
